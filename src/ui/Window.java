dcb7e44615142075b7bea1fa057ddb31dedf6f94
Mikołaj Sajewicz <107215730+mSajewiczz@users.noreply.github.com>
2025-04-09 23:12:48 +0200

    Modified few modules

--- src/ui/Window.java
+++ src/ui/Window.java
@@ -1,23 +1,48 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Window extends JFrame {
 
     public Window() {
-        setTitle("Java Snake Game");
-
-        setSize(450, 300);
-
+        setTitle("🐍 Java Snake Game 🐍");
+        setSize(1000, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
+        JLabel gameTitle = new JLabel("Welcome in the Snake Game! 🐍");
+        gameTitle.setFont(new Font("Sans-Serif", Font.BOLD, 28));
+        gameTitle.setBounds(10, 10, 1000, 28);
+
+        JButton gameStart = new JButton("Start game");
+        gameStart.setBounds(10, 58, 330, 30);
+
+        JLabel gameStartInfo = new JLabel("");
+        gameStartInfo.setBounds(10, 88, 330, 30);
+
+        add(gameStart);
+        add(gameTitle);
         setLayout(null);
+        add(gameStartInfo);
+
+
+        gameStart.addActionListener(new ActionListener() {
+
+            @Override
+
+            public void actionPerformed(ActionEvent e) {
+                gameStartInfo.setText("You've started the game!");
+            }
+
+        });
     }
 
     public static void window(String[] args) {
 
         Window frame = new Window();
-
         frame.setVisible(true);
 
     }
