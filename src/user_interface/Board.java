a46c34046a360d12a132b3ed4552476dea1550d9
Mikołaj Sajewicz <107215730+mSajewiczz@users.noreply.github.com>
2025-04-19 16:53:43 +0200

    Modified some classes

--- src/user_interface/Board.java
+++ src/user_interface/Board.java
@@ -6,8 +6,12 @@ import java.awt.*;
 public class Board extends JPanel{
 
     public Board() {
-//        JPanel gameBoard = new JPanel();
         setPreferredSize(new Dimension(600, 600));
         setBackground(Color.RED);
+        Food food = new Food();
     }
+
+
+
+
 }
--- src/user_interface/Food.java
+++ src/user_interface/Food.java
@@ -1,17 +1,26 @@
 package user_interface;
 
+import java.awt.*;
+
+
+
 public class Food {
 
     private static int x = 0;
     private static int y = 0;
 
     public Food() {
-        generateFood();
+        generatePosition();
     }
 
-
-    public static void generateFood() {
+    public static void generatePosition() {
         x = (int)(Math.random() * 601);
         y = (int)(Math.random() * 601);
     };
+
+//    @Override
+    protected void paintFood(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.drawOval(x, y, 80, 80);
+    }
 }
